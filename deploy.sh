22621f019e3ea3d0358241e6dd94215c9c8ce5e0
Eric Wieser <wieser.eric@gmail.com>
2022-11-13 17:42:53 +0000

    remove old deployment mechanism

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,4 @@
 set -e # fail on error
-
-git config --global pull.ff only
-git config --global user.name "leanprover-community-bot"
-git config --global user.email "leanprover-community@gmail.com"
-
-git clone --branch gh-pages https://github.com/leanprover-community/lean-web-editor.git
-cd lean-web-editor
-git remote add deploy "https://$GITHUB_TOKEN@github.com/leanprover-community/lean-web-editor.git"
-rm -f *.worker.js
-cd ..
-
-# After this point, we don't use any secrets in commands.
 set -x # echo commands
 
 LATEST_BROWSER_LEAN_URL=https://github.com/leanprover-community/lean/releases/download/v$LATEST_BROWSER_LEAN/lean-$LATEST_BROWSER_LEAN--browser.zip
@@ -25,13 +13,3 @@ rm leanbrowser.zip
 mv build/shell/* .
 rm -rf build/
 cd ..
-
-# push leanprover-community/lean-web-editor
-cd lean-web-editor
-git pull
-cp -a ../dist/. .
-git add -A
-git diff-index HEAD
-if [ "$github_repo" = "leanprover-community/lean-web-editor" ] && [ "$github_ref" = "refs/heads/master" ]; then
-    git diff-index --quiet HEAD || { git commit -m "lean-web-editor: $(date)" && git push deploy || exit 1; }
-fi
